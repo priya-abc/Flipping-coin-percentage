aeca08e7916e329d996dcffae6de46d4ae07b5dd
priya-abc <priyapriya6598@gmail.com>
2020-11-04 23:02:02 +0530

    Added: Singlet functionalities added

--- flipcoinperc.sh
+++ flipcoinperc.sh
@@ -7,3 +7,33 @@ echo "1)Signlet"
 echo "2)Double" 
 echo "3)Triple" 
 read option
+case $option in
+1)
+singlet=([H]=0 [T]=0)
+declare -A coin
+for i in $(seq 1 50)
+do
+flip1=$((RANDOM%2))
+if [[ $flip1 == 1 ]]
+then
+        coin[$i]="Head"
+        ((Head++))
+else
+        coin[$i]="Tail"
+        ((Tail++))
+fi
+done
+H=`expr $H \* 100 / 50 `
+echo "H : $H % "
+T=`expr $T \* 100 / 50`
+echo "T : $T %"
+declare -A coin
+coin=([H]=$H [T]=$T)
+count=0
+for i in ${!coin[@]}
+do
+        (( $i > count || count == 0 ))&& count=$i
+done
+echo "The winning probability :"
+echo "$count : ${coin[$count]} percentage%"
+;;
