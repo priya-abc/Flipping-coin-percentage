3772a3f738a07f5c49255fd7a23959c62ef2eacc
priya-abc <priyapriya6598@gmail.com>
2020-11-04 22:54:24 +0530

    Added: UserInputsadded

---
+++ flipcoinperc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#welcome message
+echo "Tossing coin winning percentage and probabilities are done in this program"
+
+echo "Enter the number of desired coin combination"
+echo "1)Signlet" 
+echo "2)Double" 
+echo "3)Triple" 
+read option
