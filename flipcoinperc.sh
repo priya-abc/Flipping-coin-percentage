af6447de8d5a09dd3ca4ffe46e359985707cb87e
priya-abc <priyapriya6598@gmail.com>
2020-11-21 20:15:27 +0530

    Added: Triplet Functionality added

--- flipcoinperc.sh
+++ flipcoinperc.sh
@@ -79,3 +79,68 @@ done
 echo "The winning combinatioon :"
 echo "$count : ${doublet[$count]} percentage %"
 ;;
+3)
+triplet=([HHH]=0 [HHT]=0 [HTH]=0 [THH]=0 [TTH]=0 [THT]=0 [HTT]=0 [TTT]=0)
+declare -A coin
+for i in $(seq 1 50)
+do
+flip1=$((RANDOM%2))
+flip2=$((RANDOM%2))
+flip3=$((RANDOM%2))
+if [[ $flip1 == 1 && $flip2 == 1 && $flip3 == 1 ]]
+then
+	coin[$i]="HHH"
+	((HHH++))
+elif [[ $flip1 == 1 && $flip2 == 1 && $flip3 == 0 ]]
+then
+        coin[$i]="HHT"
+        ((HHT++))
+elif [[ $flip1 == 1 && $flip2 == 0 && $flip3 == 1 ]]
+then
+        coin[$i]="HTH"
+        ((HTH++))
+elif [[ $flip1 == 0 && $flip2 == 1 && $flip3 == 1 ]]
+then
+        coin[$i]="THH"
+        ((THH++))
+elif [[ $flip1 == 0 && $flip2 == 0 && $flip3 == 1 ]]
+then
+        coin[$i]="TTH"
+        ((TTH++))
+elif [[ $flip1 == 0 && $flip2 == 1 && $flip3 == 0 ]]
+then
+        coin[$i]="THT"
+        ((THT++))
+elif [[ $flip1 == 1 && $flip2 == 0 && $flip3 == 0 ]]
+then
+        coin[$i]="HTT"
+        ((HTT++))
+else
+        coin[$i]="TTT"
+        ((TTT++))
+fi
+done
+HHH=`expr $HHH \* 100 / 50 `
+echo "HHH : $HHH % "
+HHT=`expr $HHT \* 100 / 50`
+echo "HHT : $HHT %" 
+HTH=`expr $HTH \* 100 / 50 `
+echo "HTH : $HTH % "
+THH=`expr $THH \* 100 / 50`
+echo "THH : $THH %"
+TTH=`expr $TTH \* 100 / 50`
+echo "TTH : $TTH %"
+THT=`expr $THT \* 100 / 50`
+echo "THT : $THT %"
+HTT=`expr $HTT \* 100 / 50`
+echo "HTT : $HTT %"
+TTT=`expr $TTT \* 100 / 50`
+echo "TTT : $TTT %"
+#echo ${coin[@]}
+declare -A triplet
+triplet=([HHH]=$HHH [HHT]=$HHT [HTH]=$HTH [THH]=$THH [TTH]=$TTH [THT]=$THT [HTT]=$HTT [TTT]=$TTT)
+count=0
+for i in ${!triplet[@]}
+do
+	(( $i > count || count == 0 ))&& count=$i
+done
